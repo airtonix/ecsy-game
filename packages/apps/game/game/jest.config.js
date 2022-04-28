8a0711f4fa386d82c5ad9184e0f548f2feee7975
Zenobius Jiricek <airtonix@users.noreply.github.com>
2022-04-29 07:07:53 +0930

    add game

---
+++ packages/apps/game/game/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'apps-game-game',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/packages/apps/game/game',
+};
